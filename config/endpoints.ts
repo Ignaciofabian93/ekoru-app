0fae1a945a72d581e1a62a0f94b03a1c247baf5a
Ignaciofabian93 <ignaciofabian93@gmail.com>
2025-08-26 16:38:29 -0400

    fix

--- config/endpoints.ts
+++ config/endpoints.ts
@@ -3,9 +3,9 @@ import { ENVIRONMENT } from "./environment";
 export const GRAPHQL_URL =
   ENVIRONMENT === "production"
     ? "https://gateway.ekoru.cl/graphql"
-    : "http://localhost:9000/graphql";
+    : "http://gateway:9000/graphql";
 
 export const REST_URL =
   ENVIRONMENT === "production"
     ? "https://gateway.ekoru.cl/session"
-    : "http://localhost:9000/session";
+    : "http://gateway:9000/session";
