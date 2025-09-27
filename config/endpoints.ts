7140fe69d5a84ab542e96e2d2e0303045952f72d
Ignaciofabian93 <ignaciofabian93@gmail.com>
2025-09-27 15:16:30 -0300

    endpoints

--- config/endpoints.ts
+++ config/endpoints.ts
@@ -1,11 +1,26 @@
 import { ENVIRONMENT } from "./environment";
 
-export const GRAPHQL_URL =
-  ENVIRONMENT === "production"
-    ? "https://qa.gateway.ekoru.cl/graphql"
-    : "http://localhost:9000/graphql";
+const getEndpoints = () => {
+  switch (ENVIRONMENT) {
+    case "production":
+      return {
+        graphql: "https://gateway.ekoru.cl/graphql",
+        rest: "https://gateway.ekoru.cl/session",
+      };
+    case "qa":
+      return {
+        graphql: "https://qa.gateway.ekoru.cl/graphql",
+        rest: "https://qa.gateway.ekoru.cl/session",
+      };
+    default:
+      return {
+        graphql: "http://localhost:9000/graphql",
+        rest: "http://localhost:9000/session",
+      };
+  }
+};
 
-export const REST_URL =
-  ENVIRONMENT === "production"
-    ? "https://qa.gateway.ekoru.cl/session"
-    : "http://localhost:9000/session";
+const endpoints = getEndpoints();
+
+export const GRAPHQL_URL = endpoints.graphql;
+export const REST_URL = endpoints.rest;
